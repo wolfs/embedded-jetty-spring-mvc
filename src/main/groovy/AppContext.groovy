07d6766bf82f643d923b69b23265e2bb5548414d
Stefan Wolf <wolfs@fs.tum.de>
2012-07-13 12:24:57 +0200

    Groovy 2.0 and maven central added

--- src/main/groovy/AppContext.groovy
+++ src/main/groovy/AppContext.groovy
@@ -1,6 +1,6 @@
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.context.annotation.ComponentScan
 
 /**
  * @author wolfs
--- src/main/groovy/controller/FooController.groovy
+++ src/main/groovy/controller/FooController.groovy
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 import groovy.json.JsonBuilder
+import groovy.transform.CompileStatic
 
 /**
  * @author wolfs
  */
 @Controller
+@CompileStatic
 final class FooController{
 
   @RequestMapping( value = "foo",method = RequestMethod.GET )
